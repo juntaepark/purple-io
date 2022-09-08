5749b8cbcfb3dc8236f0329b2c8fc0249662fbe3
da-ruu <jt0015@naver.com>
2022-09-08 12:59:49 +0900

    Odd

--- parjuntae_0909/selection test/4. 아이큐 테스트.java
+++ parjuntae_0909/selection test/4. 아이큐 테스트.java
@@ -1,31 +1,42 @@
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
 
 public class Solution {
     public int solution(String n){
 
+        int odd = 0;
+        int even = 0;
+
         String[] arr = n.split("\\s");
-        int[] values = Arrays.stream(arr).mapToInt(Integer::parseInt).toArray();
+        HashMap<Integer,Integer> hm = new HashMap<>();
 
-        List<Integer> odd = new ArrayList<>();
-        List<Integer> even = new ArrayList<>();
-
-        for(int i : values){
-            if (i % 2 != 0) odd.add(i);
-            else even.add(i);
+        int i = 0;
+        for(String s : arr){
+            hm.put(i++, Integer.parseInt(s));
         }
 
-        return odd.size() > even.size() ? even.get(0) : odd.get(0);
+        for(int j = 0; j < 3; j++){
+
+        }
+
+        for(int a : hm.keySet()){
+            if (hm.get(a) % 2 != 0) odd++;
+            else even++;
+        }
+
+
+        return 1;
     }
 
     // 아래는 테스트로 출력해 보기 위한 코드입니다.
-    //  public static void main(String[] args){
-    //      Solution s = new Solution();
-    //      String[] test = {"2 4 7 8 10", "1 2 1 1"};
+     public static void main(String[] args){
+         Solution s = new Solution();
+         String[] test = {"2 4 7 8 10", "1 2 1 1"};
 
-    //      for(int i = 0; i < test.length; i++){
-    //          System.out.println(s.solution(test[i]));
-    //      }
-    //  }
+         for(int i = 0; i < test.length; i++){
+             System.out.println(s.solution(test[i]));
+         }
+     }
 }
