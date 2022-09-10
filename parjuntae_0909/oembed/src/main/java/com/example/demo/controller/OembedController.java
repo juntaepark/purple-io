16aa8285f7691cedb6e652861043d2ab146c4cc1
da-ruu <jt0015@naver.com>
2022-09-10 17:57:53 +0900

    complete

--- parjuntae_0909/oembed/src/main/java/com/example/demo/controller/OembedController.java
+++ parjuntae_0909/oembed/src/main/java/com/example/demo/controller/OembedController.java
@@ -85,14 +85,11 @@ public class OembedController {
             String encode = URLEncoder.encode(search, StandardCharsets.UTF_8);
             String oembedUrl = createAddr(host, encode);
 
-            // httpclients를 생성해 데이터를 받아온다
             CloseableHttpClient hc = HttpClients.createDefault();
             HttpGet httpGet = new HttpGet(oembedUrl);
 
-            // httpGet Header에 content type을 지정해주고
             httpGet.addHeader("Content-Type", "application/json");
 
-            // request 요청해서 Response 받는다
             CloseableHttpResponse httpResponse = hc.execute(httpGet);
 
             result = EntityUtils.toString(httpResponse.getEntity(), "UTF-8");
@@ -113,7 +110,6 @@ public class OembedController {
 
             String[] split = url.getHost().split("\\.");
 
-            // split 데이터가 2개일 경우
             if (split.length == 2) {
                 result = split[0];
             } else if (split.length == 3) {
@@ -126,24 +122,19 @@ public class OembedController {
         return result;
     }
 
-    // 프로바이더 url + 포맷 + 인코드 합친 문자 만들기
+    // URL 생성
     public String createAddr(String host, String encode) {
         String oembedUrl = "";
 
-        // 프로바이더 url을 검색해서 해당되는 url이 나오면 멈춘다
         for (String str : lst) {
-            // 만약, url에 host(문자) 가 포함되어 있다면 멈추고 해당 데이터를 조합한다
+            
             if (str.contains(host)) {
 
-                // 프로바이더 url + ?format=json&url= + encode data
                 if (str.contains("oembed.")) {
-                    // {format}이 들어있으면 json으로 변환해준다
                     if (str.contains("{format}")) {
                         str = str.replace("{format}", "json");
                     }
 
-                    // provider_url내에 oembed.이 포함되어 있을 경우
-                    // format=json을 하지 않는다.
                     oembedUrl = str + "?url=" + encode;
 
                 } else if (str.contains("_oembed")) {
--- parjuntae_0909/oembed/src/main/resources/static/js/oembed.js
+++ parjuntae_0909/oembed/src/main/resources/static/js/oembed.js
@@ -1,175 +1,56 @@
 const getOembed = (input) => {
-  const url = "/oembed/api?search=" + input;
+  const endpoint = "/oembed/api?search=" + input;
 
-  fetch(url)
-    .then((res) => {
-      res.json()
-        .then((json) => {
-          const contentTitle = document.getElementById("content-title");
-          const contentList = document.getElementById("content-list");
+  fetch(endpoint)
+    .then((response) => response.json())
+    .then((data) => {
+      const articleTitle = document.getElementById("title-data");
+      const articleContent = document.getElementById("content-data");
 
-          contentTitle.innerHTML = "";
-          contentList.innerHTML = "";
+      articleTitle.innerHTML = "";
+      articleContent.innerHTML = "";
 
-          // 더 나은 방법은 json에 있는 데이터를 Map으로 저장하고
-          // Map으로 받아서 해당 컬렉션 안에 있는 모든 데이터를 꺼내는게 더 효율적이라 생각한다
-          // 다만, 예제에 나와있는 순서대로 정렬하고 싶어서 이 방법으로 하자!
-          let titleData = "";
-          if (json.title != null) {
-            titleData =
-              "<div><div>title</div><div>" + json.title + "</div></div>";
-              contentTitle.innerHTML = <div>title</div>
-          }
+      let title = "";
+      let content = "";
 
-          let data = "";
-          if (json.type != null)
-            data += "<li>" + "<div>type</div><div>" + json.type + "</div></li>";
-          if (json.version != null)
-            data +=
-              "<li>" + "<div>version</div><div>" + json.version + "</div></li>";
-          if (json.provider_name != null)
-            data +=
-              "<li>" +
-              "<div>provider_name</div><div>" +
-              json.provider_name +
-              "</div></li>";
-          if (json.provider_url != null)
-            data +=
-              "<li>" +
-              "<div>provider_url</div><div><a href='" +
-              json.provider_url +
-              "'>" +
-              json.provider_url +
-              "</a></div></li>";
-          if (json.author_name != null)
-            data +=
-              "<li>" +
-              "<div>author_name</div><div>" +
-              json.author_name +
-              "</div></li>";
-          if (json.author_url != null)
-            data +=
-              "<li>" +
-              "<div>author_url</div><div><a href='" +
-              json.author_url +
-              "'>" +
-              json.author_url +
-              "</a></div></li>";
-          if (json.is_plus != null)
-            data +=
-              "<li>" + "<div>is_plus</div><div>" + json.is_plus + "</div></li>";
-          if (json.html != null)
-            data +=
-              "<li>" +
-              "<div>html<br/>(" +
-              json.width +
-              "/" +
-              json.height +
-              ")</div><div><xmp>" +
-              json.html +
-              "</xmp>" +
-              json.html +
-              "</div></li>";
-          if (json.width != null)
-            data +=
-              "<li>" + "<div>width</div><div>" + json.width + "</div></li>";
-          if (json.height != null)
-            data +=
-              "<li>" + "<div>height</div><div>" + json.height + "</div></li>";
-          if (json.duration != null)
-            data +=
-              "<li>" + "<div>height</div><div>" + json.duration + "</div></li>";
-          if (json.description != null)
-            data +=
-              "<li>" +
-              "<div>height</div><div>" +
-              json.description +
-              "</div></li>";
-          if (json.thumbnail_url != null)
-            data +=
-              "<li>" +
-              "<div>thumbnail_url<br/>(" +
-              json.thumbnail_width +
-              "/" +
-              json.thumbnail_height +
-              ")</div><div><a href='" +
-              json.thumbnail_url +
-              "'>" +
-              json.thumbnail_url +
-              "</a><br/><br/><img src='" +
-              json.thumbnail_url +
-              "'/></div></li>";
-          if (json.thumbnail_width != null)
-            data +=
-              "<li>" +
-              "<div>thumbnail_width</div><div>" +
-              json.thumbnail_width +
-              "</div></li>";
-          if (json.thumbnail_height != null)
-            data +=
-              "<li>" +
-              "<div>thumbnail_height</div><div>" +
-              json.thumbnail_height +
-              "</div></li>";
-          if (json.thumbnail_url_with_play_button != null)
-            data +=
-              "<li>" +
-              "<div>thumbnail_url_with_play_button</div><div><a href='" +
-              json.thumbnail_url_with_play_button +
-              "'>" +
-              json.thumbnail_url_with_play_button +
-              "</a></div></li>";
-          if (json.upload_data != null)
-            data +=
-              "<li>" +
-              "<div>upload_data</div><div>" +
-              json.upload_data +
-              "</div></li>";
-          if (json.video_id != null)
-            data +=
-              "<li>" +
-              "<div>video_id</div><div>" +
-              json.video_id +
-              "</div></li>";
-          if (json.uri != null)
-            data += "<li>" + "<div>uri</div><div>" + json.uri + "</div></li>";
-          if (json.cache_age != null)
-            data +=
-              "<li>" +
-              "<div>cache_age</div><div>" +
-              json.cache_age +
-              "</div></li>";
+      Object.entries(data).map(([key, value]) => {
+        if (key === "title") {
+          title = "<div><div>" + key + "</div><div>" + value + "</div></div>";
+          articleTitle.innerHTML = title;
+        } else if (key === "provider_url" || key === "author_url") {
+          content +=
+            "<li><div>" +
+            key +
+            "</div><div><a href='" +
+            value +
+            "'>" +
+            value +
+            "</a></div></li>";
+        } else if (key === "html") {
+          content +=
+            "<li><div>" +
+            key +
+            "<br/>(200/200)</div><div><xmp>" +
+            value +
+            "</xmp>" +
+            value +
+            "</div></li>";
+        } else if (key === "thumbnail_url") {
+          content +=
+            "<li><div>" +
+            key +
+            "<br/>(200/200)</div><div><a href='" +
+            value +
+            "'>" +
+            value +
+            "</a><br/><br/><img src='" +
+            value +
+            "'/></div></li>";
+        } else {
+          content += "<li><div>" + key + "</div><div>" + value + "</div></li>";
+        }
 
-          render.innerHTML = data;
-          console.log(json);
-        })
-        .catch((err) => {
-          alert("유요하지 않은 주소입니다.");
-          console.log(err);
-        });
-    })
-    .catch((err) => {
-      alert("유요하지 않은 주소입니다.");
-      console.log(err);
+        articleContent.innerHTML = content;
+      });
     });
-  // .then((data) => {
-  //   console.log(data);
-  //   console.log(`jssss = ${JSON.stringify(data.response)}`);
-  //   console.log(`jtttt = ${data.response}`);
-
-  //   // let titleArea = document.getElementById("titleArea");
-  //   // let ulArea = document.getElementById("ulArea");
-  //   // titleArea.innerHTML = "";
-  //   // ulArea.innerHTML = "";
-  //   // let dataList = "";
-  //   // dataList += "<li><div>type</div><div>제이슨타입</div></li>";
-  //   // dataList += "<li><div>type</div><div>제이슨타입</div></li>";
-  //   // dataList += "<li><div>type</div><div>제이슨타입</div></li>";
-  //   // dataList += "<li><div>type</div><div>제이슨타입</div></li>";
-  //   // titleArea.innerHTML = "<div>title</div><div>제목</div>";
-  //   // ulArea.innerHTML = dataList;
-  // })
-  // .catch((err) => {
-  //   alert(`잘못 입력 하셨습니다. ${err}`);
-  // });
 };
